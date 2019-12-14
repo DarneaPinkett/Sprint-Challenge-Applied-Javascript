e89295181c8a400fc794a2647ca639e45b360aa4
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2019-12-14 13:41:07 -0500

    CardsInProgress

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(res => {
+    const data = res.data;
+    const cardInfo = cardCreator(data);
+    cardsContainer.appendChild(cardInfo)
+    console.log(res);
+})
+    .catch(err => {
+        console.log("You hit an error", err);
+    })
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function cardCreator(obj) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const span = document.createElement('span');
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    author.appendChild(span);
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imageContainer.classList.add('img-container');
+
+    headline.textContent = obj.headline;
+
+    return card
+
+}
+console.log(cardCreator())
